b7c59d42d678d73ba1f58c9a52c8e62f8a85486e
Joe Chipperfield <joechip1709@gmail.com>
2022-08-28 18:15:50 +0100

    Added new php files

---
+++ dbh.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "tm470_ema";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName)
+
+>?
